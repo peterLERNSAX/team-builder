954fd4a2f437a5acbb146adcb04cbbdb22bad727
Peter Koban <peter.koban@tu-dresden.de>
2023-03-27 13:40:09 +0200

    feat(team): Add Player model

--- teambuilder/team/models.py
+++ teambuilder/team/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    """
+    Model for a player
+    """
+    name = models.CharField(max_length=200,null=False)
+    birthdate = models.DateField(null=True)
+    position = models.CharField(max_length=3,null=True)
+    gender = models.CharField(max_length=3,null=True)
+    comment = models.CharField(max_length=1000,null=True)
+    image = models.TextField(null=True) #Base64 string
